94b4e7a50624e4c76dd03faee37c67a1ef083f08
Raju Karmuri <raju.karmuri@slu.edu>
2026-01-14 05:43:20 +0530

    Adding K distinct elements problem as well

--- Sliding Window/Java/MaximumSumSubArrayK.java
+++ Sliding Window/Java/MaximumSumSubArrayK.java
@@ -1,3 +1,8 @@
+/* Problem: Given an array of integers and a number k, find the maximum sum of a subarray of size k. */
+
+import java.util.Map;
+import java.util.HashMap;
+
 public class MaximumSumSubArrayK {
     // Time Complexity: O(N), Space Complexity: O(1)
     public int maxSumSubArrayK(int[] arr, int k) {
@@ -45,11 +50,40 @@ public class MaximumSumSubArrayK {
         return maxSum;
     }
 
+    // For K distinct elements
+    public long maxSumSubArrayKDistinct(int[] arr, int k) {
+        int n = arr.length, left = 0;
+        long maxSum = 0, windowSum = 0;
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for(int right = 0; right < n; right++) {
+            windowSum += arr[right];
+            freqMap.put(arr[right], freqMap.getOrDefault(arr[right], 0) + 1);
+
+            if(right - left + 1 > k) {
+                windowSum -= arr[left];
+                freqMap.put(arr[left], freqMap.get(arr[left]) - 1);
+                if(freqMap.get(arr[left]) == 0) {
+                    freqMap.remove(arr[left]);
+                }
+                left++;
+            }
+
+            if(right - left + 1 == k && freqMap.size() == k) {
+                maxSum = Math.max(maxSum, windowSum);
+            }
+        }
+
+        return maxSum;
+    }
+
     public static void main(String[] args) {
         MaximumSumSubArrayK solution = new MaximumSumSubArrayK();
         int[] arr = {2, 1, 5, 1, 3, 2};
+        int[] arrDistinct = {1, 5, 4, 2, 9, 9, 9, 3, 5, 1, 4};
         int k = 3;
         System.out.println("Maximum sum of subarray of size " + k + ": " + solution.maxSumSubArrayK(arr, k)); // Output: 9
         System.out.println("Maximum sum of subarray of size " + k + " (Brute Force): " + solution.maxSumSubArrayKBruteForce(arr, k)); // Output: 9
+        System.out.println("Maximum sum of subarray of size " + k + " with distinct elements: " + solution.maxSumSubArrayKDistinct(arrDistinct, k)); // Output: 15
     }
 }
